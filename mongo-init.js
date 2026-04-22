a441f80f6ebad3cf7ea4384155f9c72a814bb60b
meliosu <alex.atyurin@gmail.com>
2026-04-22 21:04:30 +0700

    add init file for mongodb

---
+++ mongo-init.js
@@ -0,0 +1,15 @@
+try {
+    rs.status();
+    print("Replica set already initialized");
+} catch (e) {
+    rs.initiate({
+      _id: "rs0",
+      members: [
+        { _id: 0, host: "mongo1:27017" },
+        { _id: 1, host: "mongo2:27017" },
+        { _id: 2, host: "mongo3:27017" }
+      ]
+    });
+
+    print("Replica set initialized successfully");
+}
