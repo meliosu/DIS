15eb1d581f7b539e645674d523bf1d6d93daa685
meliosu <alex.atyurin@gmail.com>
2026-04-22 22:18:35 +0700

    init manager main

--- manager/src/main.rs
+++ manager/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    if let Err(e) = run().await {
+        log::error!("{e}");
+    }
+}
+
+async fn run() -> anyhow::Result<()> {
+    todo!()
 }
